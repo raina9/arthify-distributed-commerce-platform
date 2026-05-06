7c185de8b51c936c2307323249067a8d4e860253
shivendramohan786@gmail.com <Shivendr@@786>
2026-05-07 00:56:16 +0530

    chore: initialize Arthify distributed commerce platform bootstrap structure

---
+++ arthify-auth-service/arthify-auth-service/src/main/java/com/arthify/auth/ArthifyAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.arthify.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArthifyAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArthifyAuthServiceApplication.class, args);
+	}
+
+}
---
+++ arthify-auth-service/arthify-auth-service/src/test/java/com/arthify/auth/ArthifyAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthify.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthifyAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
